949734908d675d680c081e9edf400f5bcaa6ab11
Fernando Chocobar <fernando.chocobar98@gmail.com>
2024-10-03 15:49:17 -0300

    folleto

---
+++ JS/menu__formasdepago.js
@@ -0,0 +1,17 @@
+function toggleMenu(menuId) {
+    // Cerrar todos los menús abiertos
+    var submenus = document.querySelectorAll('.submenu');
+    submenus.forEach(function(menu) {
+        if (menu.id !== menuId) {
+            menu.style.display = 'none';
+        }
+    });
+
+    // Alternar la visibilidad del menú seleccionado
+    var selectedMenu = document.getElementById(menuId);
+    if (selectedMenu.style.display === 'block') {
+        selectedMenu.style.display = 'none';
+    } else {
+        selectedMenu.style.display = 'block';
+    }
+}
